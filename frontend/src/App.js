eda62d6134f9687eedf74fa642a03179526dadd0
sejooo1 <sfejzagic1@etf.unsa.ba>
2025-02-07 02:18:12 +0100

    UI changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,8 @@ import VehicleList from "./components/VehicleList";
 
 function App() {
   return (
-    <div className="flex items-center space-x-4 p-4">
-      <img src="/pong.png" alt="PONG Logo" className="h-16 w-auto mr-4" />
-      <h1 className="text-3xl font-bold">Fleet Management System</h1>
+    <div>
+      <h1>Fleet Management System</h1>
       <VehicleList />
     </div>
   );
